65d95fb1188ffa78a83df76f6ce999f468dbff68
Elias Verdun02 <eleliasds@gmail.com>
2022-08-18 16:53:09 -0300

    Desafio 6

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-//Este programa es un siumulador de agenda para poder organizarse con distintas tareas. Indicando el nombre del evento,descripcion , la fecha y la hora.
-//Funciones de interaccion:
-//crearEvento(): Para crear un nuevo evento.
-//eliminarEvento(evento a eliminar): Se le pasa como parametro el evento que deseamos eliminar y lo borra de nuestro array.
-//buscarEvento(eventoBuscado): Se le pasa como parametro el evento que estamos buscando y este se mostrará en el dom. Abajo de los demas
+//Simulador tipo Todo App:
+//Por los 4 inputs se ingresan los datos del evento que el usario desea agendar. Una vez ingresado los datos , en el ultimo input apretar "Enter" y el evento se mostrara en el DOM.
+//Cada evento agendado cuenta con una "X" para poder eliminar el evento si el usuario lo desea. A travez de la funcion eliminarEvento(id), y usando el id del evento para buscarlo.
+//Faltan : Estilos, y ordenar el codigo 
+
 
 const agenda =  []
 
@@ -11,36 +11,49 @@ let dias = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado']
 let mes = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'] 
 
 class Evento{
+
+    static contadorEventos = 0;
+
     constructor(nombre,descripcion,date){
         this.nombre = nombre
         this.descripcion = descripcion
         this.date = date
-        this.fecha = `${dias[date.getDay()]}, ${date.getDate()} de ${mes[date.getMonth()]} del ${date.getFullYear()} a las ${date.getHours()}:00 hs`
+        this.id = ++Evento.contadorEventos;
     }
+
+    fecha(){
+        return `${dias[this.date.getDay()]}, ${this.date.getDate()} de ${mes[this.date.getMonth()]} del ${this.date.getFullYear()} a las ${this.date.getHours()}:${(this.date.getMinutes() >= 10) ? this.date.getMinutes() : '0'+this.date.getMinutes()} hs`
+    }
+
 }
 
+
+
+//Agendo eventos
+function agendarEventos(){
+    agenda.push(new Evento('Reunion laboral','Reunion sobre como mejorar el ambiente laboral',new Date(2022,8,12,13,12)))
+    agenda.push(new Evento('Salida familiar','Salida al parque',new Date(2022,10,11,16)))
+    agenda.push(new Evento('Medico','Turno para chequeo',new Date(2022,9,11,7)))
+    agenda.push(new Evento('Cumpleaños','fecha festiva',new Date(2022,11,27,22)))
+}
+
+agendarEventos()
+
 //Dia de hoy
-let today = document.querySelector('.date-today')
+let today = document.querySelector('.main__today')
 let date = new Date()
 today.innerHTML = `<h1>${dias[date.getDay()]} ${date.getDate()} de ${mes[date.getMonth()]} del ${date.getFullYear()}</h1>`
 
 
-//Agendo eventos
-agenda.push(new Evento('Reunion laboral','Reunion sobre como mejorar el ambiente laboral',new Date(2022,8,12,13)))
-agenda.push(new Evento('Salida familiar','Salida al parque',new Date(2022,10,11,16)))
-agenda.push(new Evento('Medico','Turno para chequeo',new Date(2022,9,11,7)))
-agenda.push(new Evento('Cumpleaños','fecha festiva',new Date(2022,12,27,22)))
-
 //mostrar
-
 const crearEventoHtml =(evento)=>{
     let eventoHtml = `
     <div class="evento">
         <ul>
             <li>${evento.nombre} </li>
             <li>${evento.descripcion} </li>
-            <li>${evento.date} </li>
-            <li>${evento.fecha} </li>
+            <li>${evento.fecha()} </li>
+            <li class="eliminar-evento" onclick="eliminarEvento(${evento.id})">X</li>
         </ul>
     </div>`
 
@@ -53,54 +66,54 @@ const cargarEventos=()=>{
         eventos += crearEventoHtml(evento)
     }
 
-    document.getElementById('eventos').innerHTML = eventos
+    document.querySelector('.eventos').innerHTML = eventos
 }
 
 cargarEventos()
 
+//Tomar valores
 
+let inputTime = document.querySelector(".main__container-input .input-time") 
 
-
-
+inputTime.addEventListener("keypress",(e)=>{
+    if(e.key === "Enter"){
+        crearEvento()
+    }
+})
 
 
 //Funciones de interaccion
 
 function crearEvento(){
-    let respuesta = true
- 
-    while(respuesta){
-    let date = new Date(prompt('Porfavor seleccione una fecha para el evento. Indicando primero el mes,despues el dia y luego el año. por ej: 8/11/2022)')
-    )
+    let arrayInputs = document.querySelectorAll('.input')
 
-    date.setHours(parseInt(prompt('A que horario?. Indicar solo la hora. por ej(14) en vez de 14:00')))
+    let fecha = new Date(arrayInputs[2].value)
 
-    let nombre = prompt('Nombre del evento')
+    let numeroMes = fecha.getDate()
+    
+    let horario = arrayInputs[3].value
+    let hora = parseInt(String(horario.substring(0,2))) 
+    let min = parseInt(String(horario.substring(3,5)))
+    
+    fecha.setDate(numeroMes + 1)//Para indicar bien laa fecha del mes, de lo contrario indica una fecha menos por el horario universal
+    fecha.setHours(hora,min)
 
-    let descripcion = prompt('Descripcion')
-
-    agenda.push(new Evento(nombre,descripcion,date))
+    agenda.push(new Evento(arrayInputs[0].value,arrayInputs[1].value,fecha))
 
     cargarEventos()
 
-    respuesta = confirm('Desea agendar otro evento?')
-   
-}
-}
-
-const buscarEvento=(nombre)=>{
-    let evento = agenda.find(evento => evento.nombre === nombre)
-
-    let div = document.querySelector('.container-agenda')
-    
-    div.innerHTML += crearEventoHtml(evento)
-    
+    arrayInputs.forEach((input)=>{
+        input.value = ''
+    })
 }
 
-const eliminarEvento=(nombre)=>{
-    let index = agenda.findIndex(evento => evento.nombre === nombre)
+
+const eliminarEvento=(id)=>{
+    console.log('probando funcion')
+    let index = agenda.findIndex(evento => evento.id == id)
 
     agenda.splice(index,1)
 
     cargarEventos()
 }
+
